5c8b0c9ed058ac928ff335561c00bf4554c17f1c
Kleadron <kleadron04@gmail.com>
2023-08-03 00:28:45 -0700

    forgot to add this

--- GameMods.hpp
+++ GameMods.hpp
@@ -39,6 +39,8 @@
 #define MOD_DISABLE_AUTOJUMP       // Disable Autojump, like old Java edition
 #define MOD_JAVA_STARTING_BLOCKS   // Replace the starting hotbar with the items from the Java b1.8 Creative mode starting hotbar
 //#define MOD_EXTRA_BLOCKS           // Adds extra unused blocks from the terrain.png file
+// Tweaks
+#define GFX_USE_QUADS		// Use for unsubstantial memory savings I guess (5-10 mb). Might add overhead.
 
 // Tests
 //#define TEST_DROPPED_ITEMS      // Allow dropped items to be dropped and collected.
